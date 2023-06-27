0026d2c7e97bbb4266e1b594001bc27de5d1b986
Ivan Gechev <ivan_banov@abv.bg>
2023-06-27 21:55:24 +0300

    Initial class

--- Mimicker/Mimic.cs
+++ Mimicker/Mimic.cs
@@ -1,7 +1,26 @@
 ﻿namespace Mimicker
 {
-    public class Mimic
+    /// <summary>
+    /// <para>Create a mimic for a specified type.</para>
+    /// <para>Example usage: <c>Mimic.This&lt;IRandomInterface&gt;()</c></para>
+    /// </summary>
+    public static class Mimic
     {
+        /// <summary>
+        /// A mimic for an interface.
+        /// </summary>
+        /// <param name="type">The type of interface to mimic.</param>
+        /// <returns>A mimic implementing the specified interface type.</returns>
+        public static object This(Type type)
+        {
+            throw new NotImplementedException();
+        }
 
+        /// <summary>
+        /// Mimic for an interface.
+        /// </summary>
+        /// <typeparam name="T">The type of interface to mimic.</typeparam>
+        /// <returns>A mimic implementing the specified interface type.</returns>
+        public static T This<T>() => (T)This(typeof(T));
     }
 }
